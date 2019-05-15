96f2460e61c1f7479970b1b46367011986e85a60
jailgreen <36865973+jailgreen@users.noreply.github.com>
2019-05-15 05:16:33 +0300

    3.11 puhelinluettelo full stack

---
+++ build/precache-manifest.2e3081604e0b72b8397e19762183c9c4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "8561e2cbc9a6a9701bce",
+    "url": "/static/js/main.ec882537.chunk.js"
+  },
+  {
+    "revision": "a70f5cec40a27d92b6fd",
+    "url": "/static/js/2.9ae1d89b.chunk.js"
+  },
+  {
+    "revision": "8561e2cbc9a6a9701bce",
+    "url": "/static/css/main.1ca33760.chunk.css"
+  },
+  {
+    "revision": "9829528e829c5a029ef5bcecdbc64b3a",
+    "url": "/index.html"
+  }
+];
